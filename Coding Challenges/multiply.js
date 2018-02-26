b633b3d24b853461f3632a130e77b67aa59b2f3e
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-02-26 12:11:08 -0700

    port fix

---
+++ Coding Challenges/multiply.js
@@ -0,0 +1,28 @@
+//tripleMult(2)(3)(3)
+
+//Write a function that can be invoked in this manner and returns the three numbers passed in multiplied together
+
+function tripleMult(callback)(callback)(callback){
+	console.log(x)
+	console.log(ycb);
+}
+
+function x(callback){
+	
+}
+
+function y(callback){
+	var x = tripleMult(x);
+	console.log(x);
+}
+function z(callback){
+
+}
+// function tripleMult(y){
+// 	console.log(tripleMult(x) * y);
+// }
+
+
+tripleMult(3)(4);
+
+
